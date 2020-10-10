88001bc79202ac8525d1828e85e08420dc1c7d7e
Dmitry Murzin <d.a.murzin@gmail.com>
2020-10-10 09:48:54 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const words = expr.split('**********')
+
+    convertBitsIntoDashesAndDots = (bits) => {
+      let result = ''
+      let index = -2
+      let letterCode = bits.slice(index)
+      while (letterCode !== '00' && letterCode !== '') {
+        switch (letterCode) {
+          case '10': { result = `.${result}`; break }
+          case '11': { result = `-${result}`; break }
+        }
+        letterCode = bits.slice(index - 2 , index)
+        index = index - 2
+      }
+      return result
+    }
+
+    morseOneWordDecode = (code) => {
+      let result = ''
+      for (let index = 0; index < code.length; index += 10) {
+        const letterCode = code.slice(index, index + 10)
+        result = `${result}${MORSE_TABLE[convertBitsIntoDashesAndDots(letterCode)]}`
+      }
+      return result
+    }
+
+    let result = ''
+    words.forEach(element => {
+      result = `${result} ${morseOneWordDecode(element)}`
+    });
+    return result.trim()
 }
 
 module.exports = {
